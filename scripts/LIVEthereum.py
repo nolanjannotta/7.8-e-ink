dbdeb03f79cc8d89faa1109bc9f757ca28fdc796
Nolan Jannotta <anjannotta@verizon.net>
2022-01-18 21:27:33 -0800

    first commit

---
+++ scripts/LIVEthereum.py
@@ -0,0 +1,18 @@
+from IT8951.display import AutoEPDDisplay
+from time import sleep
+
+
+display = AutoEPDDisplay(vcom=-2.06, rotate=args.rotate, spi_hz=24000000)
+
+def main():
+
+
+    print('Clearing display...')
+    display.clear()
+
+    print("cleared")
+
+
+
+if __name__ == "__main__":
+    main()
