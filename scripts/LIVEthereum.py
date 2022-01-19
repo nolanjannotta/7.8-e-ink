ad00186564e941a6aadbb4e5c053e456cb77d1d9
Nolan Jannotta <anjannotta@verizon.net>
2022-01-18 23:44:46 -0800

    ...

--- scripts/LIVEthereum.py
+++ scripts/LIVEthereum.py
@@ -6,51 +6,54 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 
-font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 180)
 
+
+class LIVEthereum:
+    def __init__(self):
+        self.title_font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 180)
+        self.title_font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 180)
+        self.title = "LIVEthereum"
+        self.display = AutoEPDDisplay(vcom=-2.06, rotate="CCW", spi_hz=24000000)
+
+        print('Clearing display...')
+        self.display.clear()
+        print("cleared")
 
     
 
-def layout_init(display):
+    def layout_init(self):
 
 
+        draw = ImageDraw.Draw(self.display.frame_buf)
+        # title box
 
-    message = "LIVEthereum"
+        # draw.rectangle((0, 0, display.width, 300),  outline = 0, width=5)
+        draw.line((0,300,self.display.width,300), width=4)
+        # draw.rectangle((0,303),display.width //2, 400, outline = 0, width=5)
+        draw.line((0,600,self.display.width,600), width=4)
 
-    draw = ImageDraw.Draw(display.frame_buf)
-    # title box
+        draw.line((1,1100,self.display.width,1100 ), width=4)
 
-    # draw.rectangle((0, 0, display.width, 300),  outline = 0, width=5)
-    draw.line((0,300,display.width,300), width=4)
-    # draw.rectangle((0,303),display.width //2, 400, outline = 0, width=5)
-    draw.line((0,600,display.width,600), width=4)
-    draw.line((1,1100,display.width,1100 ), width=4)
-    
-    img_width = display.frame_buf.width
-    text_width, _ = font.getsize(message)
-    text_height = 180
+        img_width = self.display.frame_buf.width
+        text_width, _ = self.title_font.getsize(self.title)
+        text_height = 180
 
-    draw_x = (img_width - text_width)//2
+        draw_x = (img_width - text_width)//2
 
-    draw_y = (300 - text_height)//2
+        draw_y = (300 - text_height)//2
 
-    # title
-    draw.text((0,350), "#", font=font)
-    draw.text((draw_x, draw_y), message, font=font)
-
-    display.draw_full(constants.DisplayModes.GLD16)
+        # title
+        
+        draw.text((draw_x, draw_y), self.title, font=self.title_font)
+        draw.text((0,350), "#", font=self.title_font)
+        self.display.draw_full(constants.DisplayModes.GLD16)
 
 
 
 
 def main():
-    display = AutoEPDDisplay(vcom=-2.06, rotate="CCW", spi_hz=24000000)
-
-    print('Clearing display...')
-    display.clear()
-    print("cleared")
-   
-    layout_init(display)
+    live_ethereum = LIVEthereum()
+    live_ethereum.layout_init()
 
 
 
