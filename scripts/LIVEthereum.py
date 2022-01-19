08967dab5269b62cf457024a377ccdae9afbbf3c
Nolan Jannotta <anjannotta@verizon.net>
2022-01-19 02:19:46 -0800

    ...

--- scripts/LIVEthereum.py
+++ scripts/LIVEthereum.py
@@ -59,14 +59,15 @@ class LIVEthereum:
         # clears a portion of the screen where the number is with padding each side
         # self.display.frame_buf.paste(0xFF, box=(0,304,self.display.width,596))
         draw.rectangle((0,350,self.display.width,550),  outline = 0, width=5)
+
         text_width, _ = self.title_font.getsize(block_number)
         self.last_text_width = text_width
 
         
         # draw.line((0,310,self.display.width,300), width=4)
         message = f"#{block_number}"
-        # draw.text((30,320),block_hash, font=self.hash_font)
-        # draw.text((0,310), message, font=self.block_font)
+        draw.text((30,320),block_hash, font=self.hash_font)
+        draw.text((0,310), message, font=self.block_font)
         
 
         
