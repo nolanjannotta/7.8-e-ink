01e0a0c89c3f1383df51d1a9d97e577196988b6b
Nolan Jannotta <anjannotta@verizon.net>
2022-01-20 09:30:29 -0800

    ...

--- scripts/main.py
+++ scripts/main.py
@@ -36,7 +36,7 @@ def main():
             seconds_since_start = (block.timestamp - start_timestamp)            
             num_tx = len(block.transactions)
             gas_price = web3.eth.gas_price / 10**9
-            live_ethereum.update_block(str(block.number),str(block_hash.hex()),format(gas_price, ".3f"), str(num_tx),date_time )
+            live_ethereum.update_block(str(block.number),str(block_hash.hex()),format(gas_price, ".3f"), str(num_tx),date_time, block.transactions)
             # live_ethereum.handle_transactions()
 
             print(f"block # {block.number} | number of transaction:  {num_tx} | at {block.timestamp}")
