788e71c8911b915e1d2f50595e317236ef399e49
Nolan Jannotta <anjannotta@verizon.net>
2022-01-19 22:29:09 -0800

    ...\

--- scripts/LIVEthereum.py
+++ scripts/LIVEthereum.py
@@ -12,7 +12,7 @@ class LIVEthereum:
     def __init__(self):
         self.display = AutoEPDDisplay(vcom=-2.06, rotate="CCW", spi_hz=24000000)
         self.title_font = ImageFont.truetype("/home/pi/7.8-e-ink/fonts/PlayfairDisplay-BlackItalic.ttf", 180)
-        self.block_font = ImageFont.truetype("/home/pi/7.8-e-ink/fonts/Zag_Bold.ttf", 210)
+        self.block_font = ImageFont.truetype("/home/pi/7.8-e-ink/fonts/Zag_Bold.ttf", 180)
         self.hash_font = ImageFont.truetype("/home/pi/7.8-e-ink/fonts/Zag_Bold.ttf", 43)
         self.gas_font = ImageFont.truetype("/home/pi/7.8-e-ink/fonts/Zag_Bold.ttf", 60)
         self.refresh_counter = 0
