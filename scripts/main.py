8f68a88deb9ec886bc9b0d901fd045f997fc8bd3
Nolan Jannotta <anjannotta@verizon.net>
2022-01-21 01:29:41 -0800

    ...

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from hexbytes import HexBytes
 
 # web3 = Web3(Web3.HTTPProvider('https://arb-mainnet.g.alchemy.com/v2/nMhcsR5Fy0pEsnb9mvzEkcvQIH2iqD7V'))
-web3 = Web3(Web3.HTTPProvider('https://eth-rinkeby.alchemyapi.io/v2/eriQZWQGeXNylGAMxvyOVIBe4JyU0Kxz'))
+web3 = Web3(Web3.HTTPProvider('https://eth-mainnet.alchemyapi.io/v2/vVidCCd2daPFQhGJWNT_ee1ogKc1jvir'))
 # web3 = Web3(Web3.WebsocketProvider('wss://eth-mainnet.alchemyapi.io/v2/tMUpxrBRib2XG1LhKdvGVql4LtGbdu58'))
 
 latest_block_filter = web3.eth.filter('latest')
@@ -38,12 +38,10 @@ def main():
             block = web3.eth.get_block(block_hash.hex())
             date_time = datetime.fromtimestamp(block.timestamp)
             num_tx = len(block.transactions)
-            pending -= num_tx
             gas_price = web3.eth.gas_price / 10**9
             eth_burned = (block.baseFeePerGas * block.gasUsed) / 10**18
 
             live_ethereum.update_block(str(block.number),str(block_hash.hex()),format(gas_price, ".3f"), str(num_tx),date_time, block.transactions, format(eth_burned, ".5f"), pending)
-
             # live_ethereum(pending_transactions)
             # live_ethereum.handle_transactions()
         time.sleep(1)
