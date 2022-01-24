48c94542546bf2f2a59d95f2ce1a52c50a3d5b04
Nolan Jannotta <anjannotta@verizon.net>
2022-01-23 19:12:58 -0800

    ...

--- scripts/LIVEthereum.py
+++ scripts/LIVEthereum.py
@@ -150,17 +150,15 @@ class LIVEthereum:
         
         # self.handle_pending(draw, block_data['num_pending'])
 
-        x_value = 20 + price_width + time_width + 20
-
+        x_value = 20 + price_width + 20 + time_width + 20
         draw.text((x_value,470), gas,font=self.get_font("Zag_Bold.ttf", 60))
-
         draw.text((x_value,375), average_gas,font=self.get_font("Zag_Bold.ttf", 35))
 
+        x_value = 125 + block_number_width + 20
         draw.text((x_value,408), miner, font=self.get_font("Zag_Bold.ttf", 60))
         
         x_value = 20 + price_width + 20
         draw.text((x_value,470),time,font=self.get_font("Zag_Bold.ttf", 60))
-
         draw.text((x_value,527),burned,font=self.get_font("Zag_Bold.ttf", 60))
 
 
