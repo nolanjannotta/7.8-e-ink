8b092b5ce9f47cb885689da1b4f3bcf2ac80ef40
Nolan Jannotta <anjannotta@verizon.net>
2022-01-24 00:32:07 -0800

    ...

--- scripts/LIVEthereum.py
+++ scripts/LIVEthereum.py
@@ -19,7 +19,7 @@ class LIVEthereum:
         
         self.connection_health = {}
         self.activity_list = deque([])
-        self.current_tracked_tx = []
+        self.current_tracked_tx = deque([])
         
 
 
@@ -241,15 +241,18 @@ class LIVEthereum:
         short_hash = f"{hash[:5]}...{hash[len(hash)-3:]}"
         message = f"-activity from {address} in {short_hash} in block #{block_number}"
         self.display.frame_buf.paste(0xFF, box=(10, 1535, 925 , self.display.height))
+
         self.current_tracked_tx.append(hash)
 
         if len(self.activity_list) < 7:
             if message not in self.activity_list:
                 self.activity_list.appendleft(message)
+                self.current_tracked_tx.appendleft(hash)
                 
         else:
             # draw.rectangle((10, 1535, 905 , self.display.height),  outline = 0, width=5)
             self.activity_list.pop()
+            self.current_tracked_tx.appendleft(hash)
             if message not in self.activity_list:
                 self.activity_list.appendleft(message)
 
