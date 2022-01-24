465059e161d51f2f39817b0a8b8c822a5d597c47
Nolan Jannotta <anjannotta@verizon.net>
2022-01-23 21:32:35 -0800

    ...

--- scripts/LIVEthereum.py
+++ scripts/LIVEthereum.py
@@ -124,7 +124,7 @@ class LIVEthereum:
 
 
         draw = ImageDraw.Draw(self.display.frame_buf)
-
+        task = asyncio.create_task(self.handle_transactions(draw,block_data['transactions'], block_data['num_tx']))
 
         # clears a portion of the screen where the number is with padding each side
         self.display.frame_buf.paste(0xFF, box=(0,254,self.display.width,596))
@@ -179,7 +179,7 @@ class LIVEthereum:
 
 
         self.display.draw_partial(constants.DisplayModes.DU)
-        await self.handle_transactions(draw,block_data['transactions'], block_data['num_tx'])
+        
 
 
         
