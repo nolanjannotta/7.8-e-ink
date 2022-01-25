bc7bb6f7ea40aced5e467bdf195cb7e87192b75a
Nolan Jannotta <anjannotta@verizon.net>
2022-01-24 22:52:22 -0800

    ...

--- scripts/LIVEthereum.py
+++ scripts/LIVEthereum.py
@@ -132,9 +132,9 @@ class LIVEthereum:
         message = f"loading transactions for #{block_number}..."
         loading_width, _ = self.get_font("Zag_Bold.ttf", 70).getsize(message)
         x_value = (910 - loading_width) // 2
-
-        draw.text((x_value, 800), message, font=self.get_font("Zag_Bold.ttf", 70))
         self.display.frame_buf.paste(0xFF, box=(0,655,self.display.width,1458))
+        draw.text((x_value, 800), message, font=self.get_font("Zag_Bold.ttf", 70))
+        
         self.display.draw_partial(constants.DisplayModes.DU)
 
 
